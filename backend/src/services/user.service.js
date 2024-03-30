23b5887d6b01fea07f0b60e44e4f8ff11c07ddaa
Mohamed El Dahtowry <salintofficial@hotmail.com>
2024-03-30 06:45:11 +0200

    Unique usernames

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -6,6 +6,8 @@ class UserService {
 
 	async createUser(username, email, password) {
 		
+		const conn = DatabaseService.conn;
+
 		if(username.length > 24) {
 			throw new ErrorEx("Username cannot be longer than 24 letters.", "auth/long-username", 400);
 		}
@@ -13,9 +15,17 @@ class UserService {
 			throw new ErrorEx("Invalid email provided.", "auth/invalid-email", 400);
 		}
 		else {
-			const hash = await wp.encP(password, "base64");
 
-			await DatabaseService.conn.query("INSERT INTO users (`username`, `password`, `email`, `bio`) VALUES (?, ?, ?, ?)", [username, hash, email, ""]);
+			const users = await conn.query("SELECT username FROM users WHERE username=?", username);
+			
+			if(users.length != 0) {
+				throw new ErrorEx("Username already taken.", "auth/username-taken", 400);
+			}
+			else {
+				const hash = await wp.encP(password, "base64");
+				await conn.query("INSERT INTO users (`username`, `password`, `email`, `bio`) VALUES (?, ?, ?, ?)", [username, hash, email, ""]);
+			}
+
 		}
 	}
 }
